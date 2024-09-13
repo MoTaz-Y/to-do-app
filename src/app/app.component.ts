9093688a29f2cfea160e35e8c9c9409911efecc1
AmeerHeiba <31767185+AmeerHeiba@users.noreply.github.com>
2024-09-13 14:23:55 +0300

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { TodoWrapperComponent } from '../app/todo-wrapper/todo-wrapper.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [TodoWrapperComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'todo-app';
